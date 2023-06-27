f7c584f0c9c4af2b62cb2ef53ff014e6dd8719e0
Omar Adel <omarxadel21@gmail.com>
2023-06-27 12:32:07 +0300

    add packages and make example work

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+var cors = require("cors");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const port = 8080;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
 });
 
 app.listen(port, () => {
